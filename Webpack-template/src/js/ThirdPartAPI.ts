0929a7f4262a8af20fc9efc72f27fb8999ccfb3c
DST001 <davidt72@yahoo.dk>
2018-12-16 19:07:45 +0100

    web

---
+++ Webpack-template/src/js/ThirdPartAPI.ts
@@ -0,0 +1,16 @@
+/*import axios from 'axios';
+  
+let Buttonhent: HTMLButtonElement = <HTMLButtonElement> document.getElementById("hent");
+let hent: HTMLDivElement = <HTMLDivElement> document.getElementById("output");
+  
+Buttonhent.addEventListener("click", MouseEvent =>{
+// Make a request for a user with a given ID
+axios.get('http://worldclockapi.com/api/jsonp/cet/now?callback=mycallback')
+.then(function (response) {
+  hent.innerHTML = JSON.stringify(response.data[0].id)
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
+});
+  })*/
